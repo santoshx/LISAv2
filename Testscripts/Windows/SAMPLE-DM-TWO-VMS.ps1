ef822fc520a81ece669ed1049dd26945abea7c15
Santosh <v-santy@microsoft.com>
2018-10-28 15:39:03 +0530

    Sample test using 2 VMs

---
+++ Testscripts/Windows/SAMPLE-DM-TWO-VMS.ps1
@@ -0,0 +1,42 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache License.
+
+<#
+.Synopsis
+	Sample test to demo usage of two VMs for DM tests
+.Description
+	Sample test to demo usage of two VMs for DM tests
+.Parameter testParams
+	Test data for this test case
+#>
+
+Param([String] $TestParams)
+
+$ErrorActionPreference = "Stop"
+$testResult = "FAIL"
+
+Function Main
+{
+	param ($VM1, $VM2)
+	$resultArr = @()
+
+	try {
+		LogMsg "$VM1"
+		LogMsg "$VM2"
+
+		$testResult = "PASS"
+
+	} catch {
+		$testResult = "FAIL"
+		$ErrorMessage =  $_.Exception.Message
+		$ErrorLine = $_.InvocationInfo.ScriptLineNumber
+		LogErr "$ErrorMessage at line: $ErrorLine"
+
+	} finally {
+		$resultArr += $testResult
+	}
+
+	return GetFinalResultHeader -resultarr $resultArr
+} # end Main
+
+Main -VM1 $allVMData[0] -VM2 $allVMData[1]
