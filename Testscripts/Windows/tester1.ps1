78f4e8d5fad47452fcce90e3d390af4622b21279
Santosh <v-santy@microsoft.com>
2018-11-05 12:07:36 +0530

    tester for RunSetupScriptOnlyOnce

---
+++ Testscripts/Windows/tester1.ps1
@@ -0,0 +1,12 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache License.
+
+<#
+.Synopsis
+
+.Description
+#>
+param([String] $TestParams)
+
+LogMsg "tester1"
+LogMsg $TestParams 
